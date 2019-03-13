6c7d36c3bd070eb6423b936a9b0fb22ca6845a5e
Weber, Pam <SortaTrek@gmail.com>
2019-03-13 14:25:27 -0500

    fixed loop for allowing user to try to enter search method 3 times

--- DocumentSearch.py
+++ DocumentSearch.py
@@ -19,51 +19,50 @@ file3 = ""
 
 # give the user up to 3 tries to enter a search string of at least one character
 searchStringTries = 1
+print ""                                                         # print a blank line for readability
 while True :
-    if searchStringTries > 3 :
-        print "3 STRIKES AND YOU'RE OUT!  Good bye."
+    if searchStringTries > 3 :                                   # kick the user out of the program after 3 tries
+        print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
         exit()        
     else :
-        print "String Try Number", searchStringTries
+        # print "String Try Number", searchStringTries
         searchString = raw_input("Enter the search term: ")     # ask user for the search string
         if len(searchString) < 1 :                              # if user didn't enter anything, give them another try
             searchStringTries = searchStringTries + 1           # increment the number of tries   
-            if searchStringTries < 3 :
+            if searchStringTries < 4 :
                 print "Enter at least one character for your search. Please try again."
         else :
             break
-print "\nSearchString =", searchString
+print "    You entered the following term for your search: ", searchString, "\n"
 
 # give the user 3 tries to select a valid search method
 searchMethodTries = 1
 while True :
-    if searchMethodTries > 3 :                                
-        print "3 STRIKES AND YOU'RE OUT!  Good bye."
+    if searchMethodTries > 3 :                                  # kick the user out of the program after 3 tries                                
+        print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
         exit()
     else :
-        print "\nMethod Try Number", searchMethodTries
-        # ask user for the search method
-        searchMethod = raw_input("Enter the number for the desired search method - 1 (String Match), 2 (Regular Expression), or 3 (Indexed): ")
+        # print "Method Try Number", searchMethodTries
+        searchMethod = raw_input("Enter the number for the desired search method - 1 (String Match), 2 (Regular Expression), or 3 (Indexed): ")  # ask user for the search method
         if len(searchMethod) < 1 :                                 
-            searchMethodTries = searchMethodTries + 1               # increment the number of tries
-            if searchStringTries < 3 :
+            searchMethodTries = searchMethodTries + 1           # increment the number of tries
+            if searchMethodTries < 4 :
                 print "You didn't enter a value.  Please try again."
         elif searchMethod < "1" or searchMethod > "3" :
-            searchMethodTries = searchMethodTries + 1               # increment the number of tries
-            if searchMethodTries < 3 :
+            searchMethodTries = searchMethodTries + 1           # increment the number of tries
+            if searchMethodTries < 4 :
                 print "You were supposed to enter 1, 2, or 3 for search method.  Please try again."
         else :
-            break
-        
-
-# print the Search Method description
-if searchMethod == "1" :
-    searchType = "StringMatch"
-elif searchMethod == "2" :
-    searchType = "RegularExpression"
-else :
-    searchType = "Indexed" 
-print "Search Type = ", searchType
+             # print the Search Method description
+            if searchMethod == "1" :
+                searchType = "StringMatch"
+            elif searchMethod == "2" :
+                searchType = "RegularExpression"
+            else :
+                searchType = "Indexed" 
+            # print "    Search Method Description = ", searchType
+            print "    You selected the following search method: " + searchMethod + " (" + searchType + ")"
+            break  
 
 # process the first file
 # **** initially, just reading and printing the file ****
@@ -99,20 +98,25 @@ except:  # print an error message and exit the program if the file can't be open
     exit()
 
 # order file matches in descending order  
-# **** using dummy assignments until coding is done ****
+# **** START using dummy assignments until coding is done ****
 file1 = fileB
 file2 = fileA
 file3 = fileC
+file1Matches = 10
+file2Matches = 7
+file3Matches = 2
+# **** END using dummy assignments until coding is done ****
 
 # print results
-print file1, " - ", file1Matches, "matches"
-print file2, " - ", file2Matches, "matches"
-print file3, " - ", file3Matches, "matches"
+print "** RESULTS LISTED IN DESCENDING ORDER **"
+print "  ", file1, " - ", file1Matches, "matches"
+print "  ", file2, " - ", file2Matches, "matches"
+print "  ", file3, " - ", file3Matches, "matches"
 
 # calculate and print run time in milliseconds
 timerStop = dt.datetime.now()
 timerTotal = (timerStop-timerStart).microseconds
-print "Elapshed Time: ", timerTotal, "ms"
+print "\nElapsed Time: ", timerTotal, "ms\n"
 
 quit()
 
