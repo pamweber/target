859ca22cc1c6eed404eda9c682c998ba43115b26
Weber, Pam <SortaTrek@gmail.com>
2019-03-14 14:49:21 -0500

    Get rid of extraneous code before starting on method 3

--- DocumentSearch.py
+++ DocumentSearch.py
@@ -11,7 +11,8 @@
 # ******************************************************************************
 
 # IMPORT functions
-import datetime as dt
+import datetime as dt                               # date time 
+import re                                           # regular expressions
 
 # SPECIFY names of the files to be read
 fileA = "french_armed_forces.txt"
@@ -36,15 +37,15 @@ while True :
         print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
         exit()        
     else :
-        # print "String Try Number", searchStringTries
-        searchString = raw_input("Enter the search term: ")     # ask user for the search string
+        # ask user for the search string
+        searchString = raw_input("Enter the search term: ")
         if len(searchString) < 1 :                              # if user didn't enter anything, give them another try
             searchStringTries = searchStringTries + 1           # increment the number of tries   
             if searchStringTries < 4 :
                 print "Enter at least one character for your search. Please try again."
         else :
-            break
-# print "    You entered the following term for your search: ", searchString, "\n"
+             # got the search string so break out of while loop
+             break
 
 # GET search method
 searchMethodTries = 1
@@ -53,8 +54,8 @@ while True :
         print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
         exit()
     else :
-        # print "Method Try Number", searchMethodTries
-        searchMethod = raw_input("Search Method: 1) String Match 2) Regular Expression 3) Indexed: ")  # ask user for the search method
+        # ask user for the search method
+        searchMethod = raw_input("Search Method: 1) String Match 2) Regular Expression 3) Indexed: ")
         if len(searchMethod) < 1 :                                 
             searchMethodTries = searchMethodTries + 1           # increment the number of tries
             if searchMethodTries < 4 :
@@ -64,56 +65,45 @@ while True :
             if searchMethodTries < 4 :
                 print "You were supposed to enter 1, 2, or 3 for search method.  Please try again."
         else :
-             # print the Search Method description
-            if searchMethod == "1" :
-                searchType = "StringMatch"
-            elif searchMethod == "2" :
-                searchType = "RegularExpression"
-            else :
-                searchType = "Indexed" 
-            # print "    Search Method Description = ", searchType
-            # print "    You selected the following search method: " + searchMethod + " (" + searchType + ")"
+             # got the search method so break out of while loop
             break  
 
 # START timer now that user has entered their responses - this eliminates user response time variable from the time measurement
 timerStart = dt.datetime.now()
 
 # READ the first file
-try:
+try :
     fhandA = open(fileA,"r")
     fileContentsA = fhandA.read()
     fhandA.close()
-except:  # print an error message and exit the program if the file can't be opened
+except :  # if the file can't be opened, print an error message and exit the program
     print "ERROR: First file (" + fileA + ") can't be opened.  Stopping program!\n"
     exit()
 
 # READ the second file
-try:
+try :
     fhandB = open(fileB,"r")
     fileContentsB = fhandB.read()
     fhandA.close()
-except:  # print an error message and exit the program if the file can't be opened
+except :  # if the file can't be opened, print an error message and exit the program
     print "ERROR: Second file (" + fileB + ") can't be opened.  Stopping program!\n"
     exit()
 
 # READ the third file
-try:
+try :
     fhandC = open(fileC,"r")
     fileContentsC = fhandC.read()
     fhandC.close()
-except:  # print an error message and exit the program if the file can't be opened
+except :  # if the file can't be opened, print an error message and exit the program
     print "ERROR: Third file (" + fileC + ") can't be opened.  Stopping program!\n"
     exit()
 
 # COUNT occurrences of search string in each file based on selected search method
 if searchMethod == "1" :
-    # print "Using Search Method 1"
     fileCountA = fileContentsA.count(searchString)
     fileCountB = fileContentsB.count(searchString)
     fileCountC = fileContentsC.count(searchString)
 elif searchMethod == "2" :
-    # print "Using Search Method 2"
-    import re
     fileCountA = len(re.findall(searchString,fileContentsA))
     fileCountB = len(re.findall(searchString,fileContentsB))
     fileCountC = len(re.findall(searchString,fileContentsC))
@@ -121,11 +111,10 @@ else :
     print "Using Search Method 3"
 
 # SORT the search string counts in descending order
-def takeSecond(elem):                                                       # little function to use second column
+def takeSecond(elem) :                                                      # a little function to retrieve second column
     return elem[1]
-counts = [(fileA, fileCountA), (fileB, fileCountB), (fileC, fileCountC)]    # fill the list (a.k.a., array)
-counts.sort(key=takeSecond,reverse=True)                                   # sort list by second column in descending order
-# print "Sorted list:", counts                                                # print list
+counts = [(fileA, fileCountA), (fileB, fileCountB), (fileC, fileCountC)]    # fill the 2 dimensional list (a.k.a., array)
+counts.sort(key=takeSecond,reverse=True)                                    # sort list by second column in descending order
 
 # PRINT the results
 print "\nSearch Results"
@@ -133,10 +122,9 @@ print "  ", counts[0][0], " - ", counts[0][1], "matches"
 print "  ", counts[1][0], " - ", counts[1][1], "matches"
 print "  ", counts[2][0], " - ", counts[2][1], "matches"
 
-# CALCULATE and print run time in milliseconds
+# CALCULATE and print runtime in milliseconds
 timerStop = dt.datetime.now()
 timerTotal = (timerStop-timerStart).microseconds
 print "\nElapsed Time: ", timerTotal, "ms\n"
 
 quit()
-
