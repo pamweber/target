ae2e14ca8573c45ad490a60baacfc0196ee042a4
Weber, Pam <SortaTrek@gmail.com>
2019-03-24 09:15:47 -0500

    Classes and Methods and Tuples, Oh My!

--- DocumentSearch.py
+++ DocumentSearch.py
@@ -1,17 +1,22 @@
 # ******************************************************************************
-# * PROGRAM NAME:       DocumentSearch.py                                      *
+# * PROGRAM NAME:       DocumentSearchClass.py                                 *
 # * PROGRAM AUTHOR:     Pam Weber                                              *
 # * LOCATION:           https://github.com/pamweber/target                     *
+# *                                                                            *
+# * CHANGES:            converted to Python 3                                  *
+# *                     converted to classes and methods for PyTest            *
+# *                                                                            *
 # * INSTRUCTIONS:                                                              *
-# *     MacOS           1) copy program and text files to local drive          *
-# *                     2) open terminal                                       *
-# *                     3) navigate to folder with program and text files      *
-# *                     4) type 'python DocumentSearch.py' to run program      *
-# *                     5) respond to prompts                                  *
-# *                     6) evaluate results                                    *
+# *     MacOS           1) copy program file to local drive                    *
+# *                     2) copy data files to /data folder on local drive      *
+# *                     3) open terminal                                       *
+# *                     4) navigate to folder with program file                *
+# *                     5) type 'python DocumentSearch.py'                     *
+# *                     6) respond to prompts                                  *
+# *                     7) evaluate results                                    *
 # ******************************************************************************
 
-# ******************************************************************************
+# ****************************************************************************** 
 # IMPORT some handy Python functions
 # ******************************************************************************
 
@@ -23,136 +28,197 @@ import re                                           # regular expressions
 # DEFINE some functions to streamline the code
 # ******************************************************************************
 
-# FUNCTION fileContents - read the contents of a file into a string
-def fileContents(fileName) :
+# FUNCTION ReadFile - read the contents of a file into a string
+def funcReadFile(fileName) :
     try :
-        fhandA = open(fileName,"r")
-        fileContents = fhandA.read()
+        fhandA = open("data/"+fileName,"r")
+        funcReadFile = fhandA.read()
         fhandA.close()
-        return fileContents
+        return funcReadFile
     except :  # if the file can't be opened, print an error message and exit the program
-        print "ERROR: First file (" + fileName + ") can't be opened.  Stopping program!\n"
+        print("ERROR: First file " + fileName + " can't be opened.  Stopping program!\n")
         exit()
 
-# FUNCTION indexSearch - search for the search string using an index search
-def indexSearch(fileContents, searchString) :
+# FUNCTION IndexSearch - search for the search string using an index search
+def funcIndexSearch(fileContents, searchTerm) :
     fileCount = 0
-    sub_len = len(searchString)
+    sub_len = len(searchTerm)
     for i in range(len(fileContents)) :
-        if fileContents[i:i+sub_len] == searchString :
+        if fileContents[i:i+sub_len] == searchTerm :
             fileCount += 1
-    indexSearch = fileCount
-    return indexSearch
+    funcIndexSearch = fileCount
+    return funcIndexSearch
     
-# FUNCTION takeSecond - retrieve the second column of a 2 dimensional list
-def takeSecond(elem) :                                                      
+# FUNCTION TakeSecond - retrieve the second column of a 2 dimensional list
+def funcTakeSecond(elem) :                                                      
     return elem[1]
 
 
 # ******************************************************************************
-# SPECIFY names of the files to be read                                        *
+# Now for the program logic set up as Methods within Classes
+#   Class:  clSearchString
+#       Method: methSearchString - ask user for a search string
+#   Class:  clSearchType
+#       Method: methSearchType - ask user for the search type
+#   Class:  clCountSort
+#       Method: methCountSort - use search string and type to count search string
+#           by selected type and then sort files into descending order
 # ******************************************************************************
 
-fileA = "french_armed_forces.txt"
-fileB = "hitchhikers.txt"
-fileC = "warp_drive.txt"
+class clSearchString(object):
+    
+    # SEARCH STRING METHOD
+    def methSearchString (self) :
+        
+        # INITIALIZE fields
+        self.searchString = ""
+        
+        # GET search string from user
+        searchStringTries = 1
+        while True :
+            if searchStringTries > 3 :                                  # kick the user out of the program after 3 tries
+                print("3 STRIKES AND YOU'RE OUT!  Good bye.\n")
+                exit()        
+            else :
+                # ask user for the search string
+                self.searchString = input("Enter the search term: ")
+                if len(self.searchString) < 1 :                         # if user didn't enter anything, give them another try
+                    searchStringTries = searchStringTries + 1           # increment the number of tries   
+                    if searchStringTries < 4 :
+                        print("Enter at least one character for your search. Please try again.")
+                else :
+                     # got the search string so break out of while loop
+                     break
+        
+        # RETURN the result
+        return self.searchString
 
+class clSearchType(object):
+    
+    # SEARCH TYPE METHOD
+    def methSearchType(self) :  
 
-# ******************************************************************************
-# INITIALIZE fields                                                            *
-# ******************************************************************************
+        # INITIALIZE fields
+        self.searchType = "" 
 
-searchMethod = "" 
-searchString = ""
-fileCountA = 0
-fileCountB = 0
-fileCountC = 0
-fileContentsA = ""
-fileContentsB = ""
-fileContentsC = ""
+        # GET search type from user
+        searchTypeTries = 1
+        while True :
+            if searchTypeTries > 3 :                                    # kick the user out of the program after 3 tries                                
+                print("3 STRIKES AND YOU'RE OUT!  Good bye.\n")
+                exit()
+            else :
+                # ask user for the search type
+                self.searchType = input("Search Method: 1) String Match 2) Regular Expression 3) Indexed: ")
+                if len(self.searchType) < 1 :                                 
+                    searchTypeTries = searchTypeTries + 1               # increment the number of tries
+                    if searchTypeTries < 4 :
+                        print("You didn't enter a value.  Please try again.")
+                elif self.searchType not in ("1", "2", "3") :
+                    searchTypeTries = searchTypeTries + 1               # increment the number of tries
+                    if searchTypeTries < 4 :
+                        print("You were supposed to enter 1, 2, or 3 for search type.  Please try again.")
+                else :
+                     # got the search type so break out of while loop
+                    break
+        
+        # RETURN the result
+        return self.searchType					
 
+class clCountSort(object):
 
-# ******************************************************************************
-# Now for the program logic                                                    *
-# ******************************************************************************
+    # COUNT AND SORT METHOD
+    def methCountSort(self,searchString, searchType) : 
 
-# ASK user for search string
-searchStringTries = 1
-print ""                                                         # print a blank line for readability
-while True :
-    if searchStringTries > 3 :                                   # kick the user out of the program after 3 tries
-        print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
-        exit()        
-    else :
-        # ask user for the search string
-        searchString = raw_input("Enter the search term: ")
-        if len(searchString) < 1 :                              # if user didn't enter anything, give them another try
-            searchStringTries = searchStringTries + 1           # increment the number of tries   
-            if searchStringTries < 4 :
-                print "Enter at least one character for your search. Please try again."
+        # INITIALIZE fields
+        self.searchString = searchString
+        self.searchType = searchType
+        fileCountA = 0
+        fileCountB = 0
+        fileCountC = 0
+        fileContentsA = ""
+        fileContentsB = ""
+        fileContentsC = ""
+                
+         # READ all three files
+        fileA = "french_armed_forces.txt"
+        fileB = "hitchhikers.txt"
+        fileC = "warp_drive.txt"
+        
+        fileContentsA = funcReadFile (fileA)
+        fileContentsB = funcReadFile (fileB)
+        fileContentsC = funcReadFile (fileC)
+        
+        # START timer here - eliminates variables in user response and file read times
+        timerStart = dt.datetime.now()
+        
+        # COUNT occurrences of search string in each file based on selected search type
+        if self.searchType == "1" :
+            # do a string match search
+            fileCountA = fileContentsA.count(self.searchString)
+            fileCountB = fileContentsB.count(self.searchString)
+            fileCountC = fileContentsC.count(self.searchString)
+        elif self.searchType == "2" :
+            # do a regex search
+            fileCountA = len(re.findall(self.searchString,fileContentsA))
+            fileCountB = len(re.findall(self.searchString,fileContentsB))
+            fileCountC = len(re.findall(self.searchString,fileContentsC))
         else :
-             # got the search string so break out of while loop
-             break
+            # do an index search (using the IndexSearch function)
+            fileCountA = funcIndexSearch(fileContentsA, self.searchString)
+            fileCountB = funcIndexSearch(fileContentsB, self.searchString)
+            fileCountC = funcIndexSearch(fileContentsC, self.searchString)
+        
+        # FILL a 2 dimensional list (a.k.a., array) with the count results
+        counts = [(fileA, fileCountA), (fileB, fileCountB), (fileC, fileCountC)]
+        
+        # SORT the list by second column in descending order
+        counts.sort(key=funcTakeSecond,reverse=True)
 
-# ASK user for search method
-searchMethodTries = 1
-while True :
-    if searchMethodTries > 3 :                                  # kick the user out of the program after 3 tries                                
-        print "3 STRIKES AND YOU'RE OUT!  Good bye.\n"
-        exit()
-    else :
-        # ask user for the search method
-        searchMethod = raw_input("Search Method: 1) String Match 2) Regular Expression 3) Indexed: ")
-        if len(searchMethod) < 1 :                                 
-            searchMethodTries = searchMethodTries + 1           # increment the number of tries
-            if searchMethodTries < 4 :
-                print "You didn't enter a value.  Please try again."
-        elif searchMethod not in ("1", "2", "3") :
-            searchMethodTries = searchMethodTries + 1           # increment the number of tries
-            if searchMethodTries < 4 :
-                print "You were supposed to enter 1, 2, or 3 for search method.  Please try again."
-        else :
-             # got the search method so break out of while loop
-            break  
+        # GATHER the search and sort results to be returned by method
+        self.fileName1 = counts[0][0]
+        self.fileCount1 = counts[0][1]
+        self.fileName2 = counts[1][0]
+        self.fileCount2 = counts[1][1]
+        self.fileName3 = counts[2][0]
+        self.fileCount3 = counts[2][1]
+  
+        # CALCULATE run time in microseconds then convert to milliseconds
+        timerStop = dt.datetime.now()
+        self.timerTotal = (timerStop - timerStart).microseconds / float(1000) 
+        
+        # RETURN the results as a tuple
+        return self.fileName1,self.fileCount1,self.fileName2,self.fileCount2,self.fileName3,self.fileCount3,self.timerTotal
 
-# Read all three files (using my fileContents function)
-fileContentsA = fileContents (fileA)
-fileContentsB = fileContents (fileB)
-fileContentsC = fileContents (fileC)
 
-# START timer here - eliminates variables in user response and file read times
-timerStart = dt.datetime.now()
+# ******************************************************************************
+# EXECUTION CODE COMMENTED OUT FOR USE WITH PYTEST
+#    1) uncomment code below to manually run the program
+#    2) from terminal type  "python3 DocumentSearch2.py" 
+# ******************************************************************************
 
-# COUNT occurrences of search string in each file based on selected search method
-if searchMethod == "1" :
-    # do a string match search
-    fileCountA = fileContentsA.count(searchString)
-    fileCountB = fileContentsB.count(searchString)
-    fileCountC = fileContentsC.count(searchString)
-elif searchMethod == "2" :
-    # do a regex search
-    fileCountA = len(re.findall(searchString,fileContentsA))
-    fileCountB = len(re.findall(searchString,fileContentsB))
-    fileCountC = len(re.findall(searchString,fileContentsC))
-else :
-    # do an index search (using my indexSearch function)
-    fileCountA = indexSearch(fileContentsA, searchString)
-    fileCountB = indexSearch(fileContentsB, searchString)
-    fileCountC = indexSearch(fileContentsC, searchString)
+# RUN the methSearchString and methSearchType methods,
+#   then pass the results to the methCountSort method
 
-# SORT the search string counts in descending order
-counts = [(fileA, fileCountA), (fileB, fileCountB), (fileC, fileCountC)]    # fill the 2 dimensional list (a.k.a., array)
-counts.sort(key=takeSecond,reverse=True)                                    # sort list by second column in descending order
+s=clSearchString()                  #instantiate the Search String class
+searchString=s.methSearchString()   #get the search string
+
+t=clSearchType()                    #instantiate the Search Type class
+searchType=t.methSearchType()       #get the search type
+
+u=clCountSort()                     #instantiate the Count Sort class
+fileName1, fileCount1, fileName2, fileCount2, fileName3, fileCount3, timerResult = u.methCountSort(searchString,searchType)
+
+# alternate format eliminates the separate calls to the Search String and Search Type methods
+# fileName1, fileCount1, fileName2, fileCount2, fileName3, fileCount3, timerResult = u.methCountSort(s.methSearchString(),t.methSearchType())
 
 # PRINT the results
-print "\nSearch Results"
-print "  ", counts[0][0], " - ", counts[0][1], "matches"
-print "  ", counts[1][0], " - ", counts[1][1], "matches"
-print "  ", counts[2][0], " - ", counts[2][1], "matches"
+print("\nSearch Results")
+print("  ", fileName1, " - ", fileCount1, "matches")
+print("  ", fileName2, " - ", fileCount2, "matches")
+print("  ", fileName3, " - ", fileCount3, "matches")
 
-# CALCULATE run time in microseconds then convert to milliseconds
-timerStop = dt.datetime.now()
-timerTotal = (timerStop-timerStart).microseconds / float(1000) 
-print "\nElapsed Time: ", timerTotal, "ms\n"
+print("\nElapsed Time: ", timerResult, "ms\n")
 
+# The quit statement doesn't work with PyTest
 quit()
